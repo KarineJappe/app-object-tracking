366514d966105a3e3bc41bb7d67439f1923e0337
karineJappe <karine_jappe@hotmail.com>
2019-11-19 14:00:37 -0300

    add home screen

---
+++ src/pages/Login.js
@@ -0,0 +1,71 @@
+import React, {Component} from 'react';
+import {
+  StyleSheet, 
+  Text, 
+  View,
+  TouchableOpacity,
+  TextInput
+} from 'react-native';
+
+export default class Login extends Component{
+  render() {
+    return (
+        <View style={styles.container}>
+        <TextInput 
+        autoCapitalize='none'
+        autoCorrect={false}
+        placeholder="Informe o codigo de rastreio.." 
+        placeholderTextColor='#999'
+        style={styles.input}
+        />
+
+       <TouchableOpacity style={styles.button}>
+         <Text style={styles.buttonText}>Pesquisar</Text>
+       </TouchableOpacity>
+
+      </View>
+     
+    );
+  } 
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#999',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 30
+  },
+ 
+  input: {
+   height: 46,
+   alignSelf: 'stretch',
+   backgroundColor: '#fff',
+   borderWidth: 1,
+   borderColor: '#ddd',
+   borderRadius: 4,
+   marginTop: 20,
+   paddingHorizontal: 15,
+  },
+  button: {
+    height: 46,
+    alignSelf: 'stretch',
+    backgroundColor: '#f5f5',
+    borderColor: '#ddd',
+    borderRadius: 4,
+    marginTop: 10,
+    justifyContent: 'center',
+    alignItems: 'center'
+
+  },
+  buttonText:{
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 11
+
+
+
+  },
+  
+});
